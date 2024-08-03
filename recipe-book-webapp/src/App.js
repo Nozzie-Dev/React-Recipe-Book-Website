c85b17ab0397658fadd473acb65e1e64c91a39aa
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-08-03 12:37:54 +0200

    images folder add

--- recipe-book-webapp/src/App.js
+++ recipe-book-webapp/src/App.js
@@ -14,12 +14,12 @@ function App() {
       steps: ["Combine the mango puree, mango essence and condenced milk.","In a seperate bowl, whip the cream till soft peaks form, about 3 minutes.","Fold the condensed-milk mixture into the whipped cream.","Pour into freezer-safe dish and cover tightly with cling wrap. Freeze for at least 5 hours or overnight."],
       prepTime: "15 minutes plus 5 hours or overnight in freezer.",
       serves: "4 cups (1l)",
-      image: "Mango-Ice-Cream.jpg"
+      image: "./images/Mango-Ice-Cream.jpg"
     },
     {
       id: 2,
       title: "Motepa Breakfast Porridge",
-      shortDescription: "traditional brown sour porridge (ting) is inspired by Batswana people",
+      shortDescription: "Traditional brown sour porridge (ting) is inspired by the Batswana people",
       ingredients: ["1 cup (250ml) maize meal","4 cups (1l) water","0.5 teaspoon (2.5ml) salt","1 tablespoon (15ml) butter","Milk & sugar/ honey to serve"],
       steps: [ "Mix the maize meal with 1 cup (250ml) cold water to make a smooth paste. Leave to ferment in a warm place for 1 day","The next day, bring 3 cups (750ml) water to boil in a large pot, and add the salt.","Whisk the fermented maize meal into the boiling water, working quickly to break up any lumps. Simmer, covered, until thick and smooth, about 20 minutes.","Stir in butter","Serve with milk and sugar or honey."],
       prepTime: "5 minutes plus 1 day for fermenting",
