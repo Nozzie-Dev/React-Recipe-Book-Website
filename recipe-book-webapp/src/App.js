31bb65dd27622ba6520e3f8f9ea6e96c868155a3
LEFITLHILE RADIPINA <lefitlhiler8@gmail.com>
2024-08-04 10:28:51 +0200

    Added navbar, and other recipes

--- recipe-book-webapp/src/App.js
+++ recipe-book-webapp/src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MyNavbar from './Navbar';
+import Home from './components/Home';
 import Recipes from './components/Recipes';
 import Details from './components/Details';
+import Category from './components/Category';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   const [recipes] = useState([
@@ -11,7 +15,7 @@ function App() {
       title: "Mango Ice-Cream",
       shortDescription: "Mango ice cream is creamy and sweet combined with the great texture of mangoes. Made from sweet mango pulp",
       ingredients: ["3 mangoes, peeled and pureed", "1 teaspoon (5ml) mango essence", "1 tin condensed milk", "1.5 cups (375 ml) fresh cream, chilled"],
-      steps: ["Combine the mango puree, mango essence and condenced milk.","In a seperate bowl, whip the cream till soft peaks form, about 3 minutes.","Fold the condensed-milk mixture into the whipped cream.","Pour into freezer-safe dish and cover tightly with cling wrap. Freeze for at least 5 hours or overnight."],
+      steps: ["Combine the mango puree, mango essence and condensed milk.", "In a separate bowl, whip the cream till soft peaks form, about 3 minutes.", "Fold the condensed-milk mixture into the whipped cream.", "Pour into freezer-safe dish and cover tightly with cling wrap. Freeze for at least 5 hours or overnight."],
       prepTime: "15 minutes plus 5 hours or overnight in freezer.",
       serves: "4 cups (1l)",
       image: ""
@@ -20,13 +24,12 @@ function App() {
       id: 2,
       title: "Motepa Breakfast Porridge",
       shortDescription: "Traditional brown sour porridge (ting) is inspired by the Batswana people",
-      ingredients: ["1 cup (250ml) maize meal","4 cups (1l) water","0.5 teaspoon (2.5ml) salt","1 tablespoon (15ml) butter","Milk & sugar/ honey to serve"],
-      steps: [ "Mix the maize meal with 1 cup (250ml) cold water to make a smooth paste. Leave to ferment in a warm place for 1 day","The next day, bring 3 cups (750ml) water to boil in a large pot, and add the salt.","Whisk the fermented maize meal into the boiling water, working quickly to break up any lumps. Simmer, covered, until thick and smooth, about 20 minutes.","Stir in butter","Serve with milk and sugar or honey."],
+      ingredients: ["1 cup (250ml) maize meal", "4 cups (1l) water", "0.5 teaspoon (2.5ml) salt", "1 tablespoon (15ml) butter", "Milk & sugar/honey to serve"],
+      steps: ["Mix the maize meal with 1 cup (250ml) cold water to make a smooth paste. Leave to ferment in a warm place for 1 day", "The next day, bring 3 cups (750ml) water to boil in a large pot, and add the salt.", "Whisk the fermented maize meal into the boiling water, working quickly to break up any lumps. Simmer, covered, until thick and smooth, about 20 minutes.", "Stir in butter", "Serve with milk and sugar or honey."],
       prepTime: "5 minutes plus 1 day for fermenting",
       cookTime: "20 minutes",
       serves: "6",
-      image: ""  ,
-    
+      image: ""
     },
     {
       id: 3,
@@ -65,12 +68,15 @@ function App() {
 
   return (
     <Router>
+      <MyNavbar />
       <Routes>
-        <Route path="/" element={<Recipes recipes={recipes} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes recipes={recipes} />} />
         <Route path="/recipe/:id" element={<Details recipes={recipes} />} />
+        <Route path="/category/:categoryName" element={<Category />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
---
+++ recipe-book-webapp/src/components/Category.js
@@ -0,0 +1,30 @@
+// src/pages/Category.js
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { recipes } from '../allrecipes';
+import './Category.css'; // Import custom CSS for styling
+
+const Category = () => {
+  const { categoryName } = useParams();
+  const categoryRecipes = recipes[categoryName] || [];
+
+  return (
+    <div>
+      <h1>{categoryName.charAt(0).toUpperCase() + categoryName.slice(1)} Recipes</h1>
+      <div className="recipes-container">
+        {categoryRecipes.map(recipe => (
+          <div key={recipe.id} className="recipe-card">
+            <h2>{recipe.name}</h2>
+            <img src={recipe.image} alt={recipe.name} className="recipe-image" />
+            <p>{recipe.description}</p>
+            <h3>Instructions:</h3>
+            <p>{recipe.instructions}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Category;
+
