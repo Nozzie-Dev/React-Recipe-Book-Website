0f8c7d6377f47ec1060ac6055ce228e37543f8fe
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-08-04 23:00:47 +0200

    Update Home.js

--- recipe-book-webapp/src/components/Home.js
+++ recipe-book-webapp/src/components/Home.js
@@ -1,15 +1,14 @@
 // Home.js
 import React, { Component } from 'react';
 import Header from '../components/Header'
-import {link} from 'react-router-dom'
+
 export default class Home extends Component{
   render() {
     return (
     <Header title= "Delicious Recipes">
      
     </Header>
-
-    
+      
     );
   } 
 }
