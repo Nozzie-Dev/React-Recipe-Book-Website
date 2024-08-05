f3bed8a99deea707b163344de108f14343492130
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-08-05 12:13:20 +0200

    images

--- recipe-book-webapp/src/components/Home.js
+++ recipe-book-webapp/src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import Header from './Header';
+import Header from '../components/Header';
+import "../../src/components/test.css"
 
 export default class Home extends Component {
   render() {
     return (
-      <Header title="Delicious Recipes" recipes={this.props.recipes} />
+      <div className='main'><Header title="Delicious Recipes" recipes={this.props.recipes} /></div>
+      
     );
   }
 }
