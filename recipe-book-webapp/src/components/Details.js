93d1ab99988d4bf5e501d893a350e597cf3219ab
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-08-05 17:41:57 +0200

    individual recipe styling

--- recipe-book-webapp/src/components/Details.js
+++ recipe-book-webapp/src/components/Details.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
+import { FaArrowLeft, FaArrowRight, FaArrowCircleLeft } from 'react-icons/fa';
 
 const Details = ({ recipes }) => {
   const { id } = useParams();
@@ -14,25 +15,47 @@ const Details = ({ recipes }) => {
   const nextRecipe = recipes[currentIndex + 1];
 
   return (
-    <div>
-      <h1>{recipe.title}</h1>
-      <img src={recipe.image} alt={recipe.title} style={{ width: '300px', height: '200px' }} />
-      <h3>Ingredients</h3>
-      <ul>
-        {recipe.ingredients.map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ul>
-      <h3>Preparation Steps</h3>
-      <ol>
-        {recipe.steps.map((step, index) => (
-          <li key={index}>{step}</li>
-        ))}
-      </ol>
-      <Link to="/"><button>Back to Recipes List</button></Link>
-      <div>
-        {previousRecipe && <Link to={`/recipe/${previousRecipe.id}`}><button>Previous</button></Link>}
-        {nextRecipe && <Link to={`/recipe/${nextRecipe.id}`}><button>Next</button></Link>}
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-md-4">
+          <h1>{recipe.title}</h1>
+          <img src={recipe.image} alt={recipe.title} className="img-fluid rounded" />
+          <div className="mt-3">
+            {previousRecipe && (
+              <Link to={`/recipe/${previousRecipe.id}`} className="btn btn-outline-secondary mr-2">
+                <FaArrowLeft /> Previous
+              </Link>
+            )}
+            {nextRecipe && (
+              <Link to={`/recipe/${nextRecipe.id}`} className="btn btn-outline-secondary">
+                Next <FaArrowRight />
+              </Link>
+            )}
+          </div>
+        </div>
+        <div className="col-md-8">
+          <h3>Ingredients</h3>
+          <ul className="list-group mb-3">
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index} className="list-group-item">
+                {ingredient}
+              </li>
+            ))}
+          </ul>
+          <h3>Preparation Steps</h3>
+          <ol className="list-group list-group-numbered">
+            {recipe.steps.map((step, index) => (
+              <li key={index} className="list-group-item">
+                {step}
+              </li>
+            ))}
+          </ol>
+          <div className="mt-3">
+            <Link to="/" className="btn btn-outline-primary">
+              <FaArrowCircleLeft /> Back to Recipes List
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
